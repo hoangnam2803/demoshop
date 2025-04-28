bfc034eee4881fefa71f7f10f55bea0e75a9ca84
hoangnam2803 <yasuodarkwind@gmail.com>
2025-04-28 15:42:26 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'base' => '/PTIT2402/demoshop/',
+    'baseURL' => 'http://localhost/PTIT2402/demoshop/',
+    'assets' => '/PTIT2402/demoshop/assets',
+    'db' => array(
+        'host'     => 'localhost',
+        'name'     => 'dbrr',
+        'username' => 'root',
+        'password' => ''
+    )
+);
